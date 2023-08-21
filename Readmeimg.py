b16a0d4e8a053f16ffd5e704968bd8742377bf0c
Ahmed Ibrahim Metawee youssef <64774266+aimtyaem@users.noreply.github.com>
2023-08-22 01:34:52 +0300

    Update Readmeimg.py
    Update

--- Readmeimg.py
+++ Readmeimg.py
@@ -1,4 +1,4 @@
 # Open the README file
 with open("README.md", "a") as f:
     # Write the markdown syntax for adding an image
-    f.write(https://github.com/aimtyaem/HC/blob/main/White%20&%20Green%20Mobile%20Mockup%20Photo%20Collage%20Beauty%20Pinterest%20Pin.jpg, "White & Green Mobile Mockup Photo Collage Beauty\n")
+    f.write(!https://github.com/aimtyaem/HC/blob/main/White%20&%20Green%20Mobile%20Mockup%20Photo%20Collage%20Beauty%20Pinterest%20Pin.jpg, "White & Green Mobile Mockup Photo Collage Beauty\n")
